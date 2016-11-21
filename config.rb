c80e2ca7a97e8ff1548b57588cfa24d277071ab3
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-11-21 13:12:10 +0100

    Initial build subtree commit

--- config.rb
+++ config.rb
@@ -17,6 +17,7 @@ page '/*.txt', layout: false
 #  which_fake_page: "Rendering a fake page with a local variable" }
 
 # General configuration
+set :relative_links, true
 
 # Reload the browser automatically whenever files change
 configure :development do
